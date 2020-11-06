c1959d4f79301f62120af78020d1b6d6e3b180ca
cat - lin <plummorgan@gmail.com>
2020-11-06 12:59:46 -0800

    made file tree and got server working

---
+++ controller/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// const homeRoutes = require('./home-routes.js');
+// const apiRoutes = require('./api');
+
+// router.use('/', homeRoutes);
+
+// router.use('/api', apiRoutes);
+
+router.use((req, res) => { //this is so if the api endpoint doesn't exist they get an error code
+    res.status(404).end();
+});
+
+module.exports = router;
