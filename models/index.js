84f6a50a8de7452dc89f8c13a2ae3cab900a0a3c
cat - lin <plummorgan@gmail.com>
2020-11-06 20:27:26 -0800

    added join statements

--- models/index.js
+++ models/index.js
@@ -4,6 +4,29 @@ const Comment = require('./Comment');
 
 //join statements will go here
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 module.exports= {
     User,
