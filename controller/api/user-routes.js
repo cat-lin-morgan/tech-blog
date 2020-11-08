82796cbe97f9fa74c09ba0dc5d3b9f628c04170f
cat - lin <plummorgan@gmail.com>
2020-11-08 00:20:04 -0800

    added single post and comments

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -4,7 +4,7 @@ const { User, Post, Comment }= require('../../models');
 //GET route to the api/users for all of the users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { excluded: ['password'] }
+        attributes: { exclude: ['password'] }
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -69,8 +69,6 @@ router.post('/', (req, res) => {
     });
 });
 
-//a POST route to log the user in
-
 //a PUT route to update the user by id
 router.put('/:id', (req, res) => {
     User.update(
@@ -79,7 +77,6 @@ router.put('/:id', (req, res) => {
         }, 
         {
         individualHooks: true,
-        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
@@ -89,7 +86,9 @@ router.put('/:id', (req, res) => {
             res.status(404).json({ message: 'No user found with this id' })
             return;
         }
-        res.json(dbUserData);
+        const userInfo = dbUserData[1][0].get({ plain: true }); //deletes the users password from the response since exlude doesn't work
+        delete userInfo.password
+        res.json(userInfo);
     })
     .catch(err => {
         console.log(err);
@@ -97,7 +96,50 @@ router.put('/:id', (req, res) => {
     });
 });
 
+//a POST route to log the user in
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at']
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that username' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
 //POST request to log the user out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 
 //DELETE route to remove the user by id
 router.delete('/:id', (req, res) => {
--- controller/home-route.js
+++ controller/home-route.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
+
+//GET request to render home page
 router.get('/', (req, res) => {
-    console.log('this is the current session', req.session);
+
+    // console.log('this is the current', req.session);
+
     Post.findAll({
-        include: [
+        include: [      
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
@@ -23,6 +27,65 @@ router.get('/', (req, res) => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', {
             posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//GET request to render login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+//after i'll need to go the login logic in the login public file
+
+//GET request to render signup page
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+//route to get to a single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        //serialize data
+        const post = dbPostData.get({ plain: true });
+        //pass data to template
+        res.render('single-post', {
+            post,
             loggedIn: req.session.loggedIn
         });
     })
